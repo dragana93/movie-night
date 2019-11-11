d26ebd79372c8653353efb1ce033dca2767959a0
draganasavanovic <dragana.savanovic@wayseven.com>
2019-11-11 11:01:45 +0100

    created movie model, movie list, movie service, navbar and style

---
+++ src/app/movie/models/movie.module.ts
@@ -0,0 +1,38 @@
+export interface Movie {
+  id: number;
+  name: string;
+  genre: string;
+  image: string;
+  releaseYear: string;
+}
+
+export const movies: Movie[] = [
+  {
+    id: 1,
+    name: "The Twilight Saga",
+    genre: "fantasy and romantic",
+    image: "assets/images/twilight.jpg",
+    releaseYear: "2008–2012"
+  },
+  {
+    id: 2,
+    name: "Gemini man",
+    genre: "action",
+    image: "assets/images/gemini.jpg",
+    releaseYear: "2019"
+  },
+  {
+    id: 3,
+    name: "Dumb & dumberer",
+    genre: "comedy",
+    image: "assets/images/dumbanddumber.jpg",
+    releaseYear: "2015"
+  },
+  {
+    id: 4,
+    name: "Maleficent",
+    genre: "action & adventure",
+    image: "assets/images/maleficent.jpg",
+    releaseYear: "2019"
+  }
+];
--- src/app/movie/movie-list/movie-list.component.ts
+++ src/app/movie/movie-list/movie-list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MovieService } from "../services/movie.service";
+import { Observable } from "rxjs";
+import { Movie } from "../models/movie.module";
 
 @Component({
-  selector: 'app-movie-list',
-  templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
+  selector: "app-movie-list",
+  templateUrl: "./movie-list.component.html",
+  styleUrls: ["./movie-list.component.css"]
 })
 export class MovieListComponent implements OnInit {
+  movies$: Observable<Movie[]>;
 
-  constructor() { }
+  constructor(private movieService: MovieService) {}
 
   ngOnInit() {
+    this.movies$ = this.movieService.getMovies();
   }
-
 }
